4c6c540f03739fbf03ef834fb1cb1ac9a4cd612a
SimonMedic <szymonpaluch@hotmail.com>
2024-03-09 15:54:53 +0100

    stylowanie logowania

--- src/js/logWindow.js
+++ src/js/logWindow.js
@@ -1,15 +1,11 @@
-<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-auth.js"></script>
-<script>
-  var firebaseConfig = {
-    apiKey: "TWÓJ_API_KEY",
-    authDomain: "TWÓJ_PROJECT_ID.firebaseapp.com",
-    projectId: "TWÓJ_PROJECT_ID",
-    storageBucket: "TWÓJ_PROJECT_ID.appspot.com",
-    messagingSenderId: "TWÓJ_MESSAGING_SENDER_ID",
-    appId: "TWÓJ_APP_ID"
-  };
+var firebaseConfig = {
+  apiKey: 'TWÓJ_API_KEY',
+  authDomain: 'TWÓJ_PROJECT_ID.firebaseapp.com',
+  projectId: 'TWÓJ_PROJECT_ID',
+  storageBucket: 'TWÓJ_PROJECT_ID.appspot.com',
+  messagingSenderId: 'TWÓJ_MESSAGING_SENDER_ID',
+  appId: 'TWÓJ_APP_ID',
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-</script>
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
