374828e38b4cde26f2dac71fddfd7701c01b17c2
Michał "Bobi" Górnik <64360492+MichalGornikk@users.noreply.github.com>
2024-03-12 19:24:39 +0100

    footer + modal fix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './css/main.css';
 import './js/fetchFilms.js';
 import './js/logWindow.js';
 import './js/mobileMenu.js';
-import './js/modal-team.js';
 import './js/modal.js';
 import './js/modalButtons.js';
 import './js/pagination.js';
 import './js/popularFilms.js';
+import './js/modal-team.js';
 
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -2,6 +2,9 @@ var modal = document.getElementById('myModal');
 
 var closeButton = modal.querySelector('.close');
 
+let button = document.querySelector('#myBtn');
+button.addEventListener('click', openModal);
+
 function openModal() {
   modal.classList.add('show');
 }
