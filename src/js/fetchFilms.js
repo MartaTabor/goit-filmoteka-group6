18cce958ec68d0c03fb8c184198adbc93544c61b
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-03-12 21:40:08 +0100

    create search

--- src/js/fetchFilms.js
+++ src/js/fetchFilms.js
@@ -5,31 +5,63 @@ import axios from 'axios';
 const API_KEY = 'AIzaSyDrsw02tJfVOg5942KrmMQDgPqT2tORfaM';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/search/movie',
+  params: { include_adult: 'false', language: 'en-US', page: '1' },
+  headers: { accept: 'application/json' },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 // zapytanie o popularne filmy
 const getTrendingMovies = async (page = 1) => {
-  try {
-    const resp = await fetch(
-      `${BASE_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`,
-    );
+  const searchParams = {
+    params: {
+      page: this.page,
+      api_key: FilmsApi.API_KEY,
+    },
+  };
 
-    if (!resp.ok) throw new Error(resp.status);
-
-    return await resp.json();
-  } catch (err) {
-    console.error(err.message);
-  }
+  return axios.get(`${FilmsApi.BASE_URL}trending/movie/week`, searchParams);
 };
+try {
+  const resp = await fetch(
+    `${BASE_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`,
+  );
+
+  if (!resp.ok) throw new Error(resp.status);
+
+  return await resp.json();
+} catch (err) {
+  console.error(err.message);
+}
 // zapytanie o gatunki
 export async function getGenres() {
-  try {
-    const response = await fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`);
-    const respGenres = await response.json();
+  const searchParams = {
+    params: {
+      page: this.page,
+      api_key: FilmsApi.API_KEY,
+    },
+  };
 
-    return respGenres;
-  } catch (error) {
-    console.log(error.message);
-  }
+  return axios.get(`${FilmsApi.BASE_URL}genre/movie/list`, searchParams);
 }
+try {
+  const response = await fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+  const respGenres = await response.json();
+
+  return respGenres;
+} catch (error) {
+  console.log(error.message);
+}
+
 // zapytanie po id
 export async function fetchTrailer(id) {
   try {
@@ -56,5 +88,3 @@ export async function fetchFilmsByQuery() {
 
   return axios.get(`${FilmsApi.BASE_URL}search/movie`, searchParams);
 }
-
-// export
