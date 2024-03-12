719e2b10b5b285814ee22630b1ae4ddc83e7f965
Michał "Bobi" Górnik <64360492+MichalGornikk@users.noreply.github.com>
2024-03-12 09:29:53 +0100

    Footer + Modal
    Miałem problem z podłączeniem pliku modal-team.js do html, więc umieściłem cały skrypt w html. Mam prośbę o sprawdzenie, czy czegoś nie pominąłem po prostu.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './css/main.css';
 
 import './js/fetchFilms.js';
-import './js/popularFilms.js';
-import './js/pagination.js';
-import './js/modal.js';
-import './js/modalButtons.js';
-import './js/modal-team.js';
 import './js/logWindow.js';
 import './js/mobileMenu.js';
+import './js/modal-team.js';
+import './js/modal.js';
+import './js/modalButtons.js';
+import './js/pagination.js';
+import './js/popularFilms.js';
 
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,1 +1,25 @@
+var modal = document.getElementById('myModal');
 
+var closeButton = modal.querySelector('.close');
+
+function openModal() {
+  modal.classList.add('show');
+}
+
+function closeModal() {
+  modal.classList.remove('show');
+}
+
+closeButton.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
