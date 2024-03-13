b2d436e7b61eb9499aa4e4bd23dcf67135afd760
Michał "Bobi" Górnik <64360492+MichalGornikk@users.noreply.github.com>
2024-03-13 18:16:07 +0100

    modal

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,19 @@
+// // Obsługa kliknięcia przycisku poprzedniej strony
+// document.getElementById('prevPage').addEventListener('click', function () {
+//   // Dodaj kod, który przewija do poprzedniej strony
+// });
 
+// // Obsługa kliknięcia przycisku następnej strony
+// document.getElementById('nextPage').addEventListener('click', function () {
+//   // Dodaj kod, który przewija do następnej strony
+// });
+
+// // Obsługa kliknięcia dowolnego przycisku numerowanej strony
+// document.querySelectorAll('.pagination-button:not(.arrows)').forEach(button => {
+//   button.addEventListener('click', function () {
+//     // Zmień aktywny przycisk na kliknięty
+//     document.querySelector('.pagination-button.active').classList.remove('active');
+//     this.classList.add('active');
+//     // Dodaj kod, który przewija do odpowiedniej strony na podstawie numeru przycisku
+//   });
+// });
