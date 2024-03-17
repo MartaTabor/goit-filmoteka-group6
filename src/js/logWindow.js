f2323e97e7cb47ebd89252063753ae9a80f817bb
Michal901 <momot1122@wp.pl>
2024-03-17 21:05:17 +0100

    Update
    Added animation in js

--- src/js/logWindow.js
+++ src/js/logWindow.js
@@ -17,6 +17,18 @@ const firebaseConfig = {
   measurementId: 'G-7R0DRVYQHE',
 };
 
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove('right-panel-active');
+});
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
