1e736e17583f76a0a2a0acc04e32e29ea9e2bbab
SimonMedic <szymonpaluch@hotmail.com>
2024-03-17 18:47:05 +0100

    big update logowania

--- src/js/logWindow.js
+++ src/js/logWindow.js
@@ -79,3 +79,29 @@ logRegBtn.addEventListener('click', () => {
   //   console.log(logRegBtn, modalRegLog);
   modalRegLog.classList.toggle('show-log-reg-window');
 });
+
+// Get the modal
+var modal = document.getElementById('userModal');
+
+// Get the button that opens the modal
+var btn = document.querySelector('.icon-user');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
